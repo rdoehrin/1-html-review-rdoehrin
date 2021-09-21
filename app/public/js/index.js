fc3871e1dd07d6968b8de2201f967a324fcf7324
rdoehrin <rdoehrin@iu.edu>
2021-09-21 15:27:51 -0400

    Added Refresh Button

--- app/public/js/index.js
+++ app/public/js/index.js
@@ -15,21 +15,25 @@ const Site = {
     }
 },
   created() {
-
+    this.fetchUser(); 
+  },
+  methods: {
+    fetchUser() {
       fetch('https://randomuser.me/api/')
-      //turns the response into json 
-      .then(response => response.json())
-      //
+      .then( response => response.json() )
       .then( (responseJson) => {
-          console.log(responseJson);
-          //assigns person to the the first person on random user
           this.person = responseJson.results[0];
       })
-
       .catch( (err) => {
-        console.error(err);
+          console.error(err);
       })
+      console.log("B");
+  },
 
+  onRefresh(event) {
+      //event.preventDefault();
+      this.fetchUser();
+  }
   }
 }
 
